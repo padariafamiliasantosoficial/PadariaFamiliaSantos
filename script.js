27b294c9d2f7cf9dfb0a9ccf3e2537d5ec69d9c7
padariafamiliasantosoficial <padariafamiliasantosoficial@gmail.com>
2025-08-31 15:34:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -261,12 +261,10 @@ function exibirProdutosPorCategoria(categoria) {
             </div>
         `;
         card.addEventListener('click', (e) => {
-            if (e.target.tagName !== 'BUTTON' && e.target.tagName !== 'INPUT') {
-                e.preventDefault();
-                exibirDetalhesProduto(produto.slug);
-                history.pushState({ slug: produto.slug }, '', `/produto/${produto.slug}`);
-            }
-        });
+    if (e.target.tagName !== 'BUTTON' && e.target.tagName !== 'INPUT') {
+        window.location.href = `/produto/${produto.slug}`; // Usa o slug na URL
+    }
+    });
         container.appendChild(card);
 
         const btnAdd = card.querySelector('.add-cart');
